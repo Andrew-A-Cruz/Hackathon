385bc1703ba3b02779c227038947aceb007cbe47
Andrew-A-Cruz <techgeek1998@gmail.com>
2023-07-13 13:04:56 -0400

    changed json file and added cart functionality

--- frontEnd/src/Store.js
+++ frontEnd/src/Store.js
@@ -1,30 +1,68 @@
 import inventory from "./candy.json";
-const Store = () => {
-  const cartStyle = {
-    color: "Red",
-  };
-  //let candy = JSON.stringify(inventory);
+import { useState, useEffect } from "react";
 
-  let candies = [];
+// for (let i = 0; i < inventory.candies.length; i++) {
+//   candies.push(
+//     "Name: " +
+//       inventory.candies[i].name +
+//       " Price: " +
+//       inventory.candies[i].Price
+//   );
+// }
+function Store() {
+  const [cart, setCart] = useState([]);
+  //const [remove, removeFromCart] = useState([])
+  const [candies, setCandies] = useState([]);
+  useEffect(() => {
+    setCandies([...inventory]);
+  }, []);
+  const renderList = candies.map((candy) => (
+    <div>
+      {candy.name + " Price: " + candy.Price}
+      <button
+        onClick={(e) => {
+          setCart([...cart, candy]);
+        }}
+        type="button"
+      >
+        Add to Cart
+      </button>
+    </div>
+  ));
+  const renderCart = cart.map((candy) => (
+    <div>
+      {candy.name + " Price: " + candy.Price}
+      {/* <button
+        onClick={(e) => {
+          setCart([...cart, candy]);
+        
+        }}
+        type="button"
+      >
+        Remove from cart
+      </button> */}
+    </div>
+  ));
 
-  for (let candy in inventory.candies) {
-    candies.push("Name: " + candy.name + " Price: " + candy.Price + "\n");
-    console.log("candy " + typeof candy);
-  }
   return (
     <>
-      <h1>DRM Store</h1>
+      <h1>DRM Candy Store</h1>
       <div>
-        <input type="text" placeholder="Search.."></input>
-        <button style={cartStyle} type="button">
-          Cart
-        </button>
+        <div>Candies:</div>
         <p></p>
-        {candies}
-        <>{}</>
+        {renderList}
       </div>
+      <div>Cart</div>
+
+      {renderCart}
+
+      {cart.reduce((total, cart) => {
+        return parseFloat(
+          Math.round((parseFloat(cart.Price) + parseFloat(total)) * 100) / 100
+        ).toFixed(2);
+      }, 0)}
     </>
   );
-};
+}
 
 export default Store;
