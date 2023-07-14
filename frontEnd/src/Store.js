e77798222620ef719dfbfa4cb8cc7df464edc85a
Andrew-A-Cruz <techgeek1998@gmail.com>
2023-07-14 11:30:22 -0400

    implimented search bar

--- frontEnd/src/Store.js
+++ frontEnd/src/Store.js
@@ -1,39 +1,41 @@
 import inventory from "./candy.json";
 import { useState, useEffect } from "react";
 import { getOrder, getOrders, addOrder } from "./rest";
-import { redirect } from "react-router-dom";
+//import { redirect } from "react-router-dom";
 
-// for (let i = 0; i < inventory.candies.length; i++) {
-//   candies.push(
-//     "Name: " +
-//       inventory.candies[i].name +
-//       " Price: " +
-//       inventory.candies[i].Price
-//   );
-// }
 function Store() {
   const [cart, setCart] = useState([]);
-  //const [remove, removeFromCart] = useState([])
   const [candies, setCandies] = useState([]);
+  const [filteredCandies, setFilteredCandies] = useState([]);
   // let audio = new Audio("./kaching.mp3");
   useEffect(() => {
     setCandies([...inventory]);
   }, []);
+  useEffect(() => {
+    let newFilterCandies = candies.filter((candy) =>
+      candy.name.toLowerCase().includes(searchInput)
+    );
+    setFilteredCandies(newFilterCandies);
+  });
 
-  const searchBar = () => {}
   const [searchInput, setSearchInput] = useState("");
   const search = (e) => {
     e.preventDefault();
-    setSearchInput(e.target.value);
+    let newE = e.target.value;
+    if (newE.includes("\\")) {
+      newE = newE.replace("\\", "/");
+      console.log("newE " + newE);
+    }
+    setSearchInput(newE);
   };
-  
+
   if (searchInput.length > 0) {
-      candies.filter((c) => {
+    candies.filter((c) => {
       return c.name.match(searchInput);
-  });
+    });
   }
 
-  const renderList = candies.map((candy) => (
+  const renderList = filteredCandies.map((candy) => (
     <div>
       <button
         onClick={(e) => {
@@ -44,26 +46,15 @@ function Store() {
       >
         Add to Cart
       </button>
-      { " ---> " + candy.name + " --- " + "Price: $" + candy.Price}
+      {" ---> " + candy.name + " --- " + "Price: $" + candy.Price}
       <p></p>
     </div>
   ));
 
   const renderCart = cart.map((candy) => (
-    <div>
-      {candy.name + " Price: " + candy.Price}
-      {/* <button
-        onClick={(e) => {
-          setCart([...cart, candy]);
-        
-        }}
-        type="button"
-      >
-        Remove from cart
-      </button> */}
-    </div>
+    <div>{candy.name + " Price: " + candy.Price}</div>
   ));
-  
+
   async function submitOrder() {
     let data = await addOrder(cart);
     setCart([]);
@@ -72,32 +63,41 @@ function Store() {
   return (
     <>
       <div clasName="hero-image">
-        <div className='hero-text'>
-      <h1 className="store-name">DRM Candy Store</h1>
-      <div>
-      <h1 className="store-h1">Candies:</h1>
-      <input
-      type="text"
-      placeholder="Search here"
-      onChange={search}
-      value={searchInput} />
-        <p></p>
-        {renderList}
-      </div>
-      <br></br>
-      <hr></hr>
-      <h1 className="store-h1">Cart</h1>
+        <div className="hero-text">
+          <h1 className="store-name">DRM Candy Store</h1>
+          <div>
+            <h1 className="store-h1">Candies:</h1>
+            <input
+              type="text"
+              placeholder="Search here"
+              onChange={search}
+              value={searchInput}
+            />
+            <p></p>
+            {renderList}
+          </div>
+          <br></br>
+          <hr></hr>
+          <h1 className="store-h1">Cart</h1>
 
-      {renderCart}
+          {renderCart}
 
-      <h2>{"Total: $" + cart.reduce((total, cart) => {
-        return parseFloat(
-          Math.round((parseFloat(cart.Price) + parseFloat(total)) * 100) / 100
-        ).toFixed(2);
-      }, 0)}</h2>
-      <br></br>
-      <button type="button" onClick={submitOrder}> Checkout </button>
-      </div>
+          <h2>
+            {"Total: $" +
+              cart.reduce((total, cart) => {
+                return parseFloat(
+                  Math.round(
+                    (parseFloat(cart.Price) + parseFloat(total)) * 100
+                  ) / 100
+                ).toFixed(2);
+              }, 0)}
+          </h2>
+          <br></br>
+          <button type="button" onClick={submitOrder}>
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
       </div>
     </>
   );
